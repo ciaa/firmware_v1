d552e3d9caa1dc5b0e698fd460437674a7cfd85f
mcerdeiro <mcerdeiro@gmail.com>
2014-04-19 22:22:43 +0200

    #4 versión inicial de los templates

---
+++ modules/template/inc/Template_File.h
@@ -0,0 +1,101 @@
+/* Copyright 2014, ACSE & CADIEEL
+ *    ACSE   : http://www.sase.com.ar/asociacion-civil-sistemas-embebidos/ciaa/
+ *    CADIEEL: http://www.cadieel.org.ar
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef _TEMPLATE_FILE_H_
+#define _TEMPLATE_FILE_H_
+/** \brief Short description of this file
+ **
+ ** Long description of this file
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup Modul Module Description
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * 
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * yyyymmdd v0.0.1 initials initial version
+ */
+
+/*==================[inclusions]=============================================*/
+
+/*==================[macros]=================================================*/
+/** \brief Short description of this macro
+ **
+ ** Long description of this macro
+ **
+ **/
+#define MACRO do
+
+/*==================[typedef]================================================*/
+/** \brief Short description of this type
+ **
+ ** Long description of this type
+ **/
+typedef unsigned char StatusType;
+
+/*==================[external data declaration]==============================*/
+/** \brief Short description of this external declaration
+ **
+ ** Long description of this external declaration
+ **/
+extern InterruptCounterType SuspendOSInterrupts_Counter ATTRIBUTES();
+
+/*==================[external functions declaration]=========================*/
+/** \brief Short description of this function
+ **
+ ** Long description of this function
+ **
+ ** \param[in] 	  Param1 Description
+ ** \param[inout] Param2 Description
+ ** \param[out]   Param3 Description
+ ** \return E_OK  OK
+ ** \return E_OS_ERROR error
+ **/
+extern ReturnType Module_FunctioName(Type Param1, Type Param2, Type Param3);
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _TEMPLATE_FILE_H_ */
+
