4a191116ee2a9b5e4a5303382615afcb3b13e2d5
mcerdeiro <mcerdeiro@gmail.com>
2014-05-21 20:30:30 +0200

    #24 revert wrong license

--- examples/blinking_make/src/blinking.c
+++ examples/blinking_make/src/blinking.c
@@ -1,69 +1,66 @@
-/* Copyright 2008, 2009, Mariano Cerdeiro
+/* Copyright 2014, ACSE & CADIEEL
+ *    ACSE   : http://www.sase.com.ar/asociacion-civil-sistemas-embebidos/ciaa/
+ *    CADIEEL: http://www.cadieel.org.ar
  *
- * This file is part of FreeOSEK.
+ * This file is part of CIAA Firmware.
  *
- * FreeOSEK is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * Linking FreeOSEK statically or dynamically with other modules is making a
- * combined work based on FreeOSEK. Thus, the terms and conditions of the GNU
- * General Public License cover the whole combination.
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
- * In addition, as a special exception, the copyright holders of FreeOSEK give
- * you permission to combine FreeOSEK program with free software programs or
- * libraries that are released under the GNU LGPL and with independent modules
- * that communicate with FreeOSEK solely through the FreeOSEK defined interface.
- * You may copy and distribute such a system following the terms of the GNU GPL
- * for FreeOSEK and the licenses of the other code concerned, provided that you
- * include the source code of that other code when and as the GNU GPL requires
- * distribution of source code.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
  *
- * Note that people who make modified versions of FreeOSEK are not obligated to
- * grant this special exception for their modified versions; it is their choice
- * whether to do so. The GNU General Public License gives permission to release
- * a modified version without this exception; this exception also makes it
- * possible to release a modified version which carries forward this exception.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
  *
- * FreeOSEK is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with FreeOSEK. If not, see <http://www.gnu.org/licenses/>.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  *
  */
 
-/** \brief Project Main Entry Point
+/** \brief Blinking example source file
  **
- ** main function implementation file
- **
- ** \file example01/src/main.c
+ ** This is a mini example of the CIAA Firmware
  **
  **/
 
-/** \addtogroup Examples Examples
+/** \addtogroup CIAA_Firmware CIAA Firmware
  ** @{ */
-/** \addtogroup Example1 Example 1
+/** \addtogroup Examples CIAA Firmware Examples
+ ** @{ */
+/** \addtogroup Blinking Blinking example source file
  ** @{ */
 
 /*
  * Initials     Name
  * ---------------------------
- * MaCe         Mariano Cerdeiro
+ *
  */
 
 /*
  * modification history (new versions first)
  * -----------------------------------------------------------
- * 20080812 v0.1.0 MaCe       - initial version
+ * yyyymmdd v0.0.1 initials initial version
  */
 
 /*==================[inclusions]=============================================*/
 #include "os.h"
 #include "stdio.h"
+#include "blinking.h"
 
 /*==================[macros and definitions]=================================*/
 
@@ -78,96 +75,81 @@
 /*==================[internal functions definition]==========================*/
 
 /*==================[external functions definition]==========================*/
-/** \brief main function
- **
- ** Project main function. This function is called after the c conformance
- ** initialisation. This function shall call the StartOS in the right
- ** Application Mode. The StartOS API shall never return.
- **
- **/
-int main
-(
-	void
-)
+int main(void)
 {
-//	printf("Hola Mundo 1\n");
-	StartOS(AppMode1);
-//	printf("Hola Mundo 2\n");
-	/* shall never return */
-	while(1);
+   uint32_t fildes;
+   printf("Hola Mundo!\n");
+   StartOS(AppMode1);
 
-	return 0;
+   /* open /dev/serial/uart0 for read and write */
+   fildes = ciaaPOSIX_open("/dev/serial/uart0", O_RDWR);
+
+   /* to avoid compile error, the variable is not used */
+   (void)fildes;
+
+   return 0;
 }
 
 void ErrorHook(void)
 {
-	printf("ErrorHook was called\n");
-	printf("Service: %d, P1: %d, P2: %d, P3: %d, RET: %d\n", OSErrorGetServiceId(), OSErrorGetParam1(), OSErrorGetParam2(), OSErrorGetParam3(), OSErrorGetRet());
-	ShutdownOS(0);
+   printf("ErrorHook was called\n");
+   printf("Service: %d, P1: %d, P2: %d, P3: %d, RET: %d\n", OSErrorGetServiceId(), OSErrorGetParam1(), OSErrorGetParam2(), OSErrorGetParam3(), OSErrorGetRet());
+   ShutdownOS(0);
 }
 
 TASK(InitTask) {
-    printf("InitTask is running\n");
+   printf("InitTask is running\n");
+   ActivateTask(TaskA);
 
-    ActivateTask(TaskA);
+   Schedule();
 
-    Schedule();
+   printf("InitTask sets TaskA Event1\n");
+   SetEvent(TaskA, Event1);
 
-    printf("InitTask sets TaskA Event1\n");
-    SetEvent(TaskA, Event1);
-
-    printf("InitTask is Terminating\n");
-    TerminateTask();
+   printf("InitTask is Terminating\n");
+   TerminateTask();
 }
 
 TASK(TaskA) {
-    printf("TaskA is running\n");
+   printf("TaskA is running\n");
 
-    printf("TaskA espera Event1\n");
-    WaitEvent(Event1);
-    printf("TaskA recibio la notficacion del Event1\n");
+   printf("TaskA espera Event1\n");
+   WaitEvent(Event1);
 
-   
-    ActivateTask(TaskB);
+   printf("TaskA recibio la notficacion del Event1\n");
+   ActivateTask(TaskB);
 
-    printf("Pedimos recurso\n");
-    GetResource(Res1);
+   printf("Pedimos recurso\n");
+   GetResource(Res1);
 
+   printf("Liberamos recurso\n");
+   ReleaseResource(Res1);
 
-    printf("Liberamos recurso\n");
-    ReleaseResource(Res1);
-
-    printf("TaskA is Terminating\n");
-    TerminateTask();
+   printf("TaskA is Terminating\n");
+   TerminateTask();
 }
 
 TASK(TaskB) {
-    printf("TaskB is running\n");
+   printf("TaskB is running\n");
 
    ActivateTask(TaskC);
    ActivateTask(TaskC);
 
    SetRelAlarm(ActivateTaskC, 350, 100);
+   printf("TaskB is Terminating\n");
 
-    printf("TaskB is Terminating\n");
-    TerminateTask();
+   TerminateTask();
 }
 
 ISR(IsrName) {
 }
 
-
 TASK(TaskC) {
-    //static int count = 0;
-
-    printf("TaskC is running\n");
-
-    
-
-    printf("TaskC is Terminating\n");
-    TerminateTask();
+   printf("TaskC is running\n");
+   printf("TaskC is Terminating\n");
+   TerminateTask();
 }
-
+/** @} doxygen end group definition */
 /** @} doxygen end group definition */
 /** @} doxygen end group definition */
 /*==================[end of file]============================================*/
