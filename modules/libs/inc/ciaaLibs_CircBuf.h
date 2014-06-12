0bdfb9b54f9d330617bd4ff40b2b65ae5d96f9e3
mcerdeiro <mcerdeiro@gmail.com>
2014-06-12 21:46:10 +0200

    #50 implement ciaaLibs_circBufWritePos and ciaaLibs_circBufUpdateTail

--- modules/libs/inc/ciaaLibs_CircBuf.h
+++ modules/libs/inc/ciaaLibs_CircBuf.h
@@ -52,7 +52,8 @@
 /*
  * modification history (new versions first)
  * -----------------------------------------------------------
- * 20140612 v0.0.6 implement ciaaLibs_circBufWritePos, ciaaLibs_circBufUpdateTail
+ * 20140612 v0.0.6 implement ciaaLibs_circBufWritePos and
+ *                 ciaaLibs_circBufUpdateTail
  * 20140612 v0.0.5 store size-1 in size to improve performance
  * 20140612 v0.0.4 rename size parameter to nbyte
  * 20140612 v0.0.3 implement ciaaLibs_circBufInit
