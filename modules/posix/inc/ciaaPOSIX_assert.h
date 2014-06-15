8667035a240f970b160428cea4e9df4ba5955dad
mcerdeiro <mcerdeiro@gmail.com>
2014-06-15 16:28:34 +0200

    #41 implement first assert

---
+++ modules/posix/inc/ciaaPOSIX_assert.h
@@ -0,0 +1,98 @@
+/* Copyright 2014, Mariano Cerdeiro
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef _CIAAPOSIX_ASSERT_H_
+#define _CIAAPOSIX_ASSERT_H_
+/** \brief ciaa POSIX assert header file
+ **
+ ** ciaa POSIX assert header file
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup POSIX POSIX Implementation
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe         Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20140615 v0.0.1 MaCe initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "ciaaPOSIX_stdio.h"
+
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==================[macros]=================================================*/
+#define ciaaPOSIX__assert(file, line, expr)                                \
+   if ((expr)==0)                                                          \
+   {                                                                       \
+      ciaaPOSIX_printf(ciaaPOSIX_assert_msg,                               \
+            (file), (line), #expr);                                        \
+      while(1);                                                            \
+   }
+
+#if 1
+#define ciaaPOSIX_assert(expr)                                             \
+   ciaaPOSIX__assert(__FILE__, __LINE__, (expr))
+#else
+#define ciaaPOSIX_assert(expr)
+#endif
+
+
+/*==================[typedef]================================================*/
+
+/*==================[external data declaration]==============================*/
+extern char const * const ciaaPOSIX_assert_msg;
+
+/*==================[external functions declaration]=========================*/
+
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+}
+#endif
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _CIAAPOSIX_ASSERT_H_ */
+
