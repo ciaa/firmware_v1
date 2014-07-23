b35832f1006516747956c0d9593ea657b7fc5230
gmuro <gmuro@fceia.unr.edu.ar>
2014-07-22 22:53:16 -0300

    #73 test ciaaModbus_process invalid function. Read Input Registers: invalid quantity of registers

--- modules/modbus/test/utest/src/test_ciaaModbusSlave.c
+++ modules/modbus/test/utest/src/test_ciaaModbusSlave.c
@@ -63,7 +63,7 @@
 /*==================[internal data declaration]==============================*/
 
 ciaaModbus_cmdLst0x04Type ciaaModbus_cmdLst0x04[] = {
-   { { 0x0000, 0x0000 }, NULL /** <= last entry */ }
+{ { 0x0000, 0x0000 }, NULL /** <= last entry */ }
 };
 
 ciaaModbus_cmdLst0x06Type ciaaModbus_cmdLst0x06[] = {
@@ -99,6 +99,48 @@ void tearDown(void) {
 void doNothing(void) {
 }
 
+/**** Helper Functions ****/
+
+/** \brief Read Integer from modbus
+ **
+ ** As described in modbus specification, the modbus uses a bigendian format to
+ ** transmit integers. This function shall be used to access integers.
+ **
+ ** \param[in] add address of the first byte of the integer to be read.
+ **
+ **/
+#define tst_readInt(add) \
+   ( (((uint8_t*)(add))[0] << 8) | (((uint8_t*)(add))[1]))
+
+
+/** \brief Write Integer in modbus buffer
+ **
+ ** As described in modbus specification, the modbus uses a bigendian format to
+ ** transmit integers. This function shall be used to access integers.
+ **
+ ** \param[in] add address of the first byte of the integer to be write.
+ ** \param[in] num integer to be write.
+ **
+ **/
+#define tst_writeInt(add,num) \
+   do {                                                        \
+      (((uint8_t*)(add))[0] = ((uint16_t)(num) >> 8) & 0XFF);  \
+      (((uint8_t*)(add))[1] = ((uint16_t)(num) >> 0) & 0XFF);  \
+   }while (0)
+
+
+
+int32_t tst_modbusPDUReadInputRegister(uint8_t *buf,
+      uint16_t startAddress,
+      uint16_t quantityOfRegisters)
+{
+   buf[0] = 0x04;
+   tst_writeInt(&buf[1],startAddress);
+   tst_writeInt(&buf[3],quantityOfRegisters);
+
+   return 5;
+}
+
 
 /** \brief Test ciaaModbus_init
  **
@@ -108,7 +150,61 @@ void test_ciaaModbus_init_01(void)
    ciaaPOSIX_open_ExpectAndReturn("/test/device", O_RDWR, 1);
 
    ciaaModbus_init("/test/device");
-}
+} /* end test_ciaaModbus_init_01 */
+
+
+/** \brief Test ciaaModbus_process
+ **
+ ** Invalid function code
+ **
+ **/
+void test_ciaaModbus_process_01(void)
+{
+   uint8_t buf[256] = {0};
+   uint8_t response[] = {0x80, 0x01};
+   int32_t ret = 5;
+
+   ret = ciaaModbus_process(buf, ret);
+
+   TEST_ASSERT_EQUAL_INT8(2, ret);
+   TEST_ASSERT_EQUAL_INT8_ARRAY(response, buf, 2);
+} /* end test_ciaaModbus_process_01 */
+
+
+
+/** \brief Test ciaaModbus_process
+ **
+ ** Test read input registers
+ ** invalid quantity of registers (> 0x007D)
+ ** invalid quantity of registers (< 0x0001)
+ **/
+void test_ciaaModbus_process_02(void)
+{
+   uint8_t buf[256];
+   uint8_t response[] = {0x84, 0x03};
+   int32_t ret;
+
+   /* read input register: quantity > 0x007D */
+   ret = tst_modbusPDUReadInputRegister(buf, 0, 0X007E);
+
+   ret = ciaaModbus_process(buf, ret);
+
+   TEST_ASSERT_EQUAL_INT8(2, ret);
+   TEST_ASSERT_EQUAL_INT8_ARRAY(response, buf, 2);
+
+   /* read input register: quantity < 0x0001 */
+   ret = tst_modbusPDUReadInputRegister(buf, 0, 0X0000);
+
+   ret = ciaaModbus_process(buf, ret);
+
+   TEST_ASSERT_EQUAL_INT8(2, ret);
+   TEST_ASSERT_EQUAL_INT8_ARRAY(response, buf, 2);
+
+} /* end test_ciaaModbus_process_02 */
+
+
+
+
 
 
 
