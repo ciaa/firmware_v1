103d49612731db0e648b08cd2c874d2b6cb6fe57
mcerdeiro <mcerdeiro@gmail.com>
2014-08-04 20:34:56 +0200

    #93 implement first system test

---
+++ modules/systests/receive/src/receive.c
@@ -0,0 +1,154 @@
+/* Copyright 2014, Mariano Cerdeiro
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** \brief Receive Test source file
+ **
+ ** This is receive test of the CIAA Firmware
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup SysTest CIAA Firmware System Test
+ ** @{ */
+/** \addtogroup Receive Receive System Test
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe         Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20140804 v0.0.1 initials initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "os.h"
+#include "ciaaPOSIX_stdio.h"
+#include "ciaaPOSIX_string.h"
+#include "ciaak.h"
+#include "receive.h"
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+/** \brief file descriptor of the uart to read from */
+static int32_t fd_uart;
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+int main(void)
+{
+   StartOS(AppMode1);
+   return 0;
+}
+
+void ErrorHook(void)
+{
+   ciaaPOSIX_printf("ErrorHook was called\n");
+   ciaaPOSIX_printf("Service: %d, P1: %d, P2: %d, P3: %d, RET: %d\n", OSErrorGetServiceId(), OSErrorGetParam1(), OSErrorGetParam2(), OSErrorGetParam3(), OSErrorGetRet());
+   ShutdownOS(0);
+}
+
+TASK(InitTask)
+{
+   /* init CIAA kernel and devices */
+   ciaak_start();
+
+   /* open UART connected to USB bridge (FT2232) */
+   /* TODO in the future this shall be received over a command over modbus */
+   fd_uart = ciaaPOSIX_open("/dev/serial/uart/1", O_RDWR);
+
+   /* change baud rate for uart usb */
+   /* TODO in the future this shall be received over a command over modbus */
+   ciaaPOSIX_ioctl(fd_uart, ciaaPOSIX_IOCTL_SET_BAUDRATE, (void *)ciaaBAUDRATE_115200);
+
+   /* change FIFO TRIGGER LEVEL for uart usb */
+   /* TODO in the future this shall be received over a command over modbus */
+   ciaaPOSIX_ioctl(fd_uart, ciaaPOSIX_IOCTL_SET_FIFO_TRIGGER_LEVEL, (void *)ciaaFIFO_TRIGGER_LEVEL3);
+
+
+   /* end InitTask and continue with receive task */
+   ChainTask(ReceiveTask);
+}
+
+TASK(ReceiveTask)
+{
+   uint8_t buf[20];
+   int32_t ret;
+   uint8_t check = '0';
+   uint32_t loopi;
+
+   while(1)
+   {
+      /* wait for any character ... */
+      ret = ciaaPOSIX_read(fd_uart, buf, 20);
+
+      for (loopi = 0; loopi < ret; loopi++)
+      {
+         if (buf[loopi] != check)
+         {
+            while(1) {
+               /* there has been an error :( */
+               /* TODO report this to the application in the pc? */
+            }
+         }
+
+         /* increment check */
+         check++;
+
+         if (check > '9')
+         {
+            check = '0';
+         }
+      }
+   }
+
+   TerminateTask();
+}
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+
