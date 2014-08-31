c2650d090a8c4c1214e49a56f295a4201b59194c
Mariano Cerdeiro <mcerdeiro@gmail.com>
2014-08-31 11:20:58 +0200

    #11 improve ci scripting

--- modules/tools/scripts/Module.pm
+++ modules/tools/scripts/Module.pm
@@ -1,6 +1,11 @@
 #!/usr/bin/perl
 
 package Module;
+
+use strict;
+use warnings;
+use File::Find;
+
 use Source;
 
 sub new
@@ -10,6 +15,18 @@ sub new
       _name => shift
    };
    print "Module name $self->{_name}\n";
+
+   opendir my $files, "modules/$self->{_name}/src/" or print "Module without files.\n";
+   while (defined (my $file = readdir $files)) {
+      if ( ($file eq ".") || ($file eq "..") )
+      {
+         next;
+      }
+      my $obj = new Source($file);
+   }
+
+   closedir $files;
+
    bless $self, $class;
    return $self;
 }
--- modules/tools/scripts/Source.pm
+++ modules/tools/scripts/Source.pm
@@ -1,3 +1,15 @@
+#!/usr/bin/perl
+
 package Source;
+
+sub new {
+   my $class = shift;
+   my $self = {
+      _name => shift
+   };
+   print "File $self->{_name}\n";
+   bless $self, $class;
+   return $self;
+}
 
 1;
