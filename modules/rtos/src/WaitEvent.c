9f9c030b80074d6f0b72a9b9bded78a8a19a9194
mcerdeiro <mcerdeiro@gmail.com>
2014-10-19 12:05:51 +0200

    #128 fix bug in WaitEvent also replace tabs with 3 spaces

--- modules/rtos/src/WaitEvent.c
+++ modules/rtos/src/WaitEvent.c
@@ -1,4 +1,4 @@
-/* Copyright 2008, 2009 Mariano Cerdeiro
+/* Copyright 2008, 2009, 2014 Mariano Cerdeiro
  * Copyright 2014, ACSE & CADIEEL
  *      ACSE: http://www.sase.com.ar/asociacion-civil-sistemas-embebidos/ciaa/
  *      CADIEEL: http://www.cadieel.org.ar
@@ -58,6 +58,8 @@
 /*
  * modification history (new versions first)
  * -----------------------------------------------------------
+ * 20141019 v0.1.4 MaCe use spaces instead tabs
+ *                      set CONTEXT_SYS before seting invalid task
  * 20090330 v0.1.3 MaCe add NO_EVENTS evaluation
  * 20090130 v0.1.2 MaCe add OSEK_MEMMAP check
  * 20081113 v0.1.1 KLi  Added memory layout attribute macros
@@ -83,106 +85,109 @@
 #if (NO_EVENTS == DISABLE)
 StatusType WaitEvent
 (
-	EventMaskType Mask
+   EventMaskType Mask
 )
 {
-	/* \req OSEK_SYS_3.18: The system service StatusType
-	  * WaitEvent ( EventMaskType Mask ) shall be defined */
+   /* \req OSEK_SYS_3.18: The system service StatusType
+    * WaitEvent ( EventMaskType Mask ) shall be defined */
 
-	volatile uint8	flag = 1;
+   volatile uint8	flag = 1;
 
-	/* \req OSEK_SYS_3.18.3 Possible return values in Standard mode is E_OK */
-	StatusType ret = E_OK;
+   /* \req OSEK_SYS_3.18.3 Possible return values in Standard mode is E_OK */
+   StatusType ret = E_OK;
 
 #if (ERROR_CHECKING_TYPE == ERROR_CHECKING_EXTENDED)
-	if ( !TasksConst[GetRunningTask()].ConstFlags.Extended )
-	{
-		/* \req OSEK_SYS_3.18.4-1/3 Extra possible return values in Extended mode
-		  * are E_OS_ACCESS, E_OS_RESOURCE, E_OS_CALLEVEL */
-		ret = E_OS_ACCESS;
-	}
-	else if ( GetCallingContext() != CONTEXT_TASK )
-	{
-		/* \req OSEK_SYS_3.18.4-2/3 Extra possible return values in Extended mode
-		  * are E_OS_ACCESS, E_OS_RESOURCE, E_OS_CALLEVEL */
-		ret = E_OS_CALLEVEL;
-	}
-	else if ( TasksVar[GetRunningTask()].Resources != 0 )
-	{
-		/* \req OSEK_SYS_3.18.4-3/3 Extra possible return values in Extended mode
-		  * are E_OS_ACCESS, E_OS_RESOURCE, E_OS_CALLEVEL */
-		ret = E_OS_RESOURCE;
-	}
-	else
+   if ( !TasksConst[GetRunningTask()].ConstFlags.Extended )
+   {
+      /* \req OSEK_SYS_3.18.4-1/3 Extra possible return values in Extended mode
+       * are E_OS_ACCESS, E_OS_RESOURCE, E_OS_CALLEVEL */
+      ret = E_OS_ACCESS;
+   }
+   else if ( GetCallingContext() != CONTEXT_TASK )
+   {
+      /* \req OSEK_SYS_3.18.4-2/3 Extra possible return values in Extended mode
+       * are E_OS_ACCESS, E_OS_RESOURCE, E_OS_CALLEVEL */
+      ret = E_OS_CALLEVEL;
+   }
+   else if ( TasksVar[GetRunningTask()].Resources != 0 )
+   {
+      /* \req OSEK_SYS_3.18.4-3/3 Extra possible return values in Extended mode
+       * are E_OS_ACCESS, E_OS_RESOURCE, E_OS_CALLEVEL */
+      ret = E_OS_RESOURCE;
+   }
+   else
 #endif
-	{
-		/* enter to critical code */
-		IntSecure_Start();
+   {
+      /* enter to critical code */
+      IntSecure_Start();
 
-		if ( Mask & TasksVar[GetRunningTask()].Events )
-		{
-			/* finish cirtical code */
-	      IntSecure_End();
-		}
-		else
-		{
-			/* \req OSEK_SYS_3.18.1 The state of the calling task is set to waiting,
-			  * unless at least one of the events specified in Mask has already been
-			  * set */
-			TasksVar[GetRunningTask()].Flags.State = TASK_ST_WAITING;
+      if ( Mask & TasksVar[GetRunningTask()].Events )
+      {
+         /* finish cirtical code */
+         IntSecure_End();
+      }
+      else
+      {
+         /* \req OSEK_SYS_3.18.1 The state of the calling task is set to waiting,
+          * unless at least one of the events specified in Mask has already been
+          * set */
+         TasksVar[GetRunningTask()].Flags.State = TASK_ST_WAITING;
 
-			/* set wait mask */
-			TasksVar[GetRunningTask()].EventsWait = Mask;
+         /* set wait mask */
+         TasksVar[GetRunningTask()].EventsWait = Mask;
 
-			/* save actual task context */
-			SaveContext(GetRunningTask());
+         /* save actual task context */
+         SaveContext(GetRunningTask());
 
-			if (flag)
-			{
+         if (flag)
+         {
 
-				/* execute this code only ones */
-				flag = 0;
+            /* execute this code only ones */
+            flag = 0;
 
-				/* remove of the Ready List */
-				RemoveTask(GetRunningTask());
+            /* remove of the Ready List */
+            RemoveTask(GetRunningTask());
 
-				/* set running task to invalid */
-         	SetRunningTask(INVALID_TASK);
+            /* set system context */
+            SetActualContext(CONTEXT_SYS);
 
-				/* finish cirtical code */
-		      IntSecure_End();
+            /* set running task to invalid */
+            SetRunningTask(INVALID_TASK);
 
-				/* OSEK_SYS_3.18.2 This call enforces rescheduling, if the wait
-				  * condition occurs. If rescheduling takes place, the internal
-				  * resource of the task is released while the task is in the
-				  * waiting state */
-				(void)Schedule();
-			}
-			else
-			{
-				/* finish critical code */
-				IntSecure_End();
-			}
-		}
-	}
+            /* finish cirtical code */
+            IntSecure_End();
+
+            /* OSEK_SYS_3.18.2 This call enforces rescheduling, if the wait
+             * condition occurs. If rescheduling takes place, the internal
+             * resource of the task is released while the task is in the
+             * waiting state */
+            (void)Schedule();
+         }
+         else
+         {
+            /* finish critical code */
+            IntSecure_End();
+         }
+      }
+   }
 
 #if ( (ERROR_CHECKING_TYPE == ERROR_CHECKING_EXTENDED) && \
       (HOOK_ERRORHOOK == ENABLE) )
-	/* \req OSEK_ERR_1.3-11/xx The ErrorHook hook routine shall be called if a
-	  * system service returns a StatusType value not equal to E_OK.*/
-	/* \req OSEK_ERR_1.3.1-11/xx The hook routine ErrorHook is not called if a
-	  * system service is called from the ErrorHook itself. */
-	if ( ( ret != E_OK ) && (ErrorHookRunning != 1))
-	{
-		SetError_Api(OSServiceId_WaitEvent);
-		SetError_Param1(Mask);
-		SetError_Ret(ret);
-		SetError_Msg("ActivateTask returns != than E_OK");
-		SetError_ErrorHook();
-	}
+   /* \req OSEK_ERR_1.3-11/xx The ErrorHook hook routine shall be called if a
+    * system service returns a StatusType value not equal to E_OK.*/
+   /* \req OSEK_ERR_1.3.1-11/xx The hook routine ErrorHook is not called if a
+    * system service is called from the ErrorHook itself. */
+   if ( ( ret != E_OK ) && (ErrorHookRunning != 1))
+   {
+      SetError_Api(OSServiceId_WaitEvent);
+      SetError_Param1(Mask);
+      SetError_Ret(ret);
+      SetError_Msg("ActivateTask returns != than E_OK");
+      SetError_ErrorHook();
+   }
 #endif
 
-	return ret;
+   return ret;
 }
 #endif /* #if (NO_EVENTS == DISABLE) */
 
