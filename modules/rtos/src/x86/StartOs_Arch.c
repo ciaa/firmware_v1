16fd9a5b0cd2199828a4a110be2203432fa0c7fb
Esteban Volentini <evolentini@gmail.com>
2014-10-26 15:45:34 -0300

    Definition necessary to compile in MAC OS

--- modules/rtos/src/x86/StartOs_Arch.c
+++ modules/rtos/src/x86/StartOs_Arch.c
@@ -73,7 +73,9 @@
 #include <signal.h>
 
 /*==================[macros and definitions]=================================*/
-
+#ifndef MAP_ANONYMOUS
+  #define MAP_ANONYMOUS MAP_ANON
+#endif
 /*==================[internal data declaration]==============================*/
 
 /*==================[internal functions declaration]=========================*/
