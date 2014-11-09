7d7481fe37cb0b9c553e387cf7c4b1851887d391
Mariano Cerdeiro <mcerdeiro@gmail.com>
2014-11-09 21:07:13 +0100

    #147 improve rm 05

--- modules/rtos/tst/ctest/src/ctest_rm_05.c
+++ modules/rtos/tst/ctest/src/ctest_rm_05.c
@@ -31,7 +31,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- */ 
+ */
 
 /** \brief FreeOSEK Os Conformance Test for the Resource Managment, Test Sequence 5
  **
@@ -82,117 +82,117 @@ const uint32f SequenceCounterOk = MAX_SEQUENCE;
 /*==================[external functions definition]==========================*/
 int main
 (
-	void
+   void
 )
 {
-	/* start OS in AppMode 1 */
-	StartOS(AppMode1);
+   /* start OS in AppMode 1 */
+   StartOS(AppMode1);
 
-	/* shall never return */
-	while(1);
+   /* shall never return */
+   while(1);
 
-	return 0;
+   return 0;
 }
 
 TASK(Task1)
 {
-	StatusType ret;
+   StatusType ret;
 
-	Sequence(0);
-	/* enable interrupts ISR2 and ISR3 */
-	/* nothing to do */
+   Sequence(0);
+   /* enable interrupts ISR2 and ISR3 */
+   /* nothing to do */
 
-	Sequence(1);
-	/* \treq RM_02 nmf B1B2E1E2 e Call GetResource() from a task with invalid
-	 * resource ID
-	 *
-	 * \result Service returns E_OS_ID
-	 */
-	ret = GetResource(INVALID_RESOURCE);
-	ASSERT(RM_02, ret != E_OS_ID);
+   Sequence(1);
+   /* \treq RM_02 nmf B1B2E1E2 e Call GetResource() from a task with invalid
+    * resource ID
+    *
+    * \result Service returns E_OS_ID
+    */
+   ret = GetResource(INVALID_RESOURCE);
+   ASSERT(RM_02, ret != E_OS_ID);
 
-	Sequence(2);
-	/*  trigger ISR 2 */
-	TriggerISR2();
+   Sequence(2);
+   /*  trigger ISR 2 */
+   TriggerISR2();
 
 #if ( ISR_CATEGORY_3 == ENABLE )
-	/* trigger ISR 3 */
-	TriggerISR3();
+   /* trigger ISR 3 */
+   TriggerISR3();
 #endif /* #if ( ISR_CATEGORY_3 == ENABLE ) */
 
-	Sequence(5);
-	/* \treq RM_12 nmf B1B2E1E2 e Call ReleaseResource() from a task with a resource
-	 * which is not occupied
-	 *
-	 * \result Service returns E_OS_NOFUNC
-	 */
-	ret = ReleaseResource(Resource1);
-	ASSERT(RM_12, ret != E_OS_NOFUNC);
+   Sequence(5);
+   /* \treq RM_12 nmf B1B2E1E2 e Call ReleaseResource() from a task with a resource
+    * which is not occupied
+    *
+    * \result Service returns E_OS_NOFUNC
+    */
+   ret = ReleaseResource(Resource1);
+   ASSERT(RM_12, ret != E_OS_NOFUNC);
 
-	Sequence(6);
-	/* \treq RM_09 nmf B1B2E1E2 e Call ReleaseResource() from a task with an
-	 * invalid resource ID
-	 *
-	 * \result Service returns E_OS_ID
-	 */
-	ret = ReleaseResource(INVALID_RESOURCE);
-	ASSERT(RM_09, ret != E_OS_ID);
+   Sequence(6);
+   /* \treq RM_09 nmf B1B2E1E2 e Call ReleaseResource() from a task with an
+    * invalid resource ID
+    *
+    * \result Service returns E_OS_ID
+    */
+   ret = ReleaseResource(INVALID_RESOURCE);
+   ASSERT(RM_09, ret != E_OS_ID);
 
-	ASSERT(OTHER, 0);
+   ASSERT(OTHER, 0);
 
-	Sequence(7);
+   Sequence(7);
 
-	/* evaluate conformance tests */
-	ConfTestEvaluation();
+   /* evaluate conformance tests */
+   ConfTestEvaluation();
 
-	/* finish the conformance test */
-	ConfTestFinish();
+   /* finish the conformance test */
+   ConfTestFinish();
 }
 
 /* This task is not used, only to change the scheduling police */
 TASK(Task2)
 {
-	while(1);
-	TerminateTask();
+   while(1);
+   TerminateTask();
 }
 
 ISR(ISR2)
 {
-	StatusType ret;
+   StatusType ret;
 
-	Sequence(3);
-    /* \treq RM_03 nmf B1B2E1E2 e Call GetResource() from ISR category 2
-	 *
-	 * \result Service returns E_OS_CALLEVEL
-	 */
-    /* this test is an error in the specification see #40
-     * Issue in TestPlan RM_03 Call GetResource and ReleaseResource from ISR2
-     */
-	ret = GetResource(Resource1);
-	ASSERT(RM_03, ret != E_OK);
+   Sequence(3);
+   /* \treq RM_03 nmf B1B2E1E2 e Call GetResource() from ISR category 2
+    *
+    * \result Service returns E_OS_CALLEVEL
+    */
+   /* this test is an error in the specification see #40
+    * Issue in TestPlan RM_03 Call GetResource and ReleaseResource from ISR2
+    */
+   ret = GetResource(Resource1);
+   ASSERT(RM_03, ret != E_OK);
 
-	Sequence(4);
-	/* \treq RM_10 nmf B1B2E1E2 e Call ReleaseResource() from ISR category 2
-	 *
-	 * \result Service returns E_OS_CALLEVEL
-	 */
-    /* this test is an error in the specification see #40
-     * Issue in TestPlan Call GetResource and ReleaseResource from ISR2
-     */
-	ret = ReleaseResource(Resource1);
-	ASSERT(RM_10, ret != E_OK);
+   Sequence(4);
+   /* \treq RM_10 nmf B1B2E1E2 e Call ReleaseResource() from ISR category 2
+    *
+    * \result Service returns E_OS_CALLEVEL
+    */
+   /* this test is an error in the specification see #40
+    * Issue in TestPlan Call GetResource and ReleaseResource from ISR2
+    */
+   ret = ReleaseResource(Resource1);
+   ASSERT(RM_10, ret != E_OK);
 }
 
 #if ( ISR_CATEGORY_3 == ENABLE )
 ISR(ISR3)
 {
-	StatusType ret;
+   StatusType ret;
 
-	EnterISR();
+   EnterISR();
 
-	/* ISR3 are not supported by FreeOSEK OS*/
+   /* ISR3 are not supported by FreeOSEK OS*/
 
-	LeaveISR();
+   LeaveISR();
 }
 #endif /* #if ( ISR_CATEGORY_3 == ENABLE ) */
 
