dd9d93bbc982ad90ab29795bf0d3c32d3c8f599b
Mariano Cerdeiro <mcerdeiro@gmail.com>
2014-11-10 21:03:11 +0100

    #148 improve ClearEvent

--- modules/rtos/src/ClearEvent.c
+++ modules/rtos/src/ClearEvent.c
@@ -1,4 +1,4 @@
-/* Copyright 2008, 2009 Mariano Cerdeiro
+/* Copyright 2008, 2009, 2014 Mariano Cerdeiro
  * Copyright 2014, ACSE & CADIEEL
  *      ACSE: http://www.sase.com.ar/asociacion-civil-sistemas-embebidos/ciaa/
  *      CADIEEL: http://www.cadieel.org.ar
@@ -58,6 +58,8 @@
 /*
  * modification history (new versions first)
  * -----------------------------------------------------------
+ * 20141109 v0.1.6 MaCe ClearEvent shall E_OS_CALLEVEL is called from any
+ *                      context != task
  * 20090330 v0.1.3 MaCe add NO_EVENTS evaluation
  * 20090128 v0.1.2 MaCe add OSEK_MEMMAP check
  * 20081113 v0.1.1 KLi  Added memory layout attribute macros
@@ -83,61 +85,61 @@
 #if (NO_EVENTS == DISABLE)
 StatusType ClearEvent
 (
-	EventMaskType Mask
+   EventMaskType Mask
 )
 {
-	/* \req OSEK_SYS_3.16 The system service StatusType
-	 * ClearEvent ( EventMaskType Mask ) shall be defined */
+   /* \req OSEK_SYS_3.16 The system service StatusType
+    * ClearEvent ( EventMaskType Mask ) shall be defined */
 
-	/* \req OSEK_SYS_3.16.2 Possible return values in Standard mode is E_OK */
-	StatusType ret = E_OK;
+   /* \req OSEK_SYS_3.16.2 Possible return values in Standard mode is E_OK */
+   StatusType ret = E_OK;
 
 #if (ERROR_CHECKING_TYPE == ERROR_CHECKING_EXTENDED)
-	if ( !TasksConst[GetRunningTask()].ConstFlags.Extended )
-	{
-		/* \req OSEK_SYS_3.16.3-1/2 Extra possible return values in Extended
-		 * mode are E_OS_ACCESS, E_OS_CALLEVEL */
-		ret = E_OS_ACCESS;
-	}
-	else if ( GetCallingContext() != CONTEXT_TASK )
-	{
-		/* \req OSEK_SYS_3.16.3-2/2 Extra possible return values in Extended
-		 * mode are E_OS_ACCESS, E_OS_CALLEVEL */
-		ret = E_OS_CALLEVEL;
-	}
-	else
+   if ( !TasksConst[GetRunningTask()].ConstFlags.Extended )
+   {
+      /* \req OSEK_SYS_3.16.3-1/2 Extra possible return values in Extended
+       * mode are E_OS_ACCESS, E_OS_CALLEVEL */
+      ret = E_OS_ACCESS;
+   }
+   else if ( GetCallingContext() != CONTEXT_TASK )
+   {
+      /* \req OSEK_SYS_3.16.3-2/2 Extra possible return values in Extended
+       * mode are E_OS_ACCESS, E_OS_CALLEVEL */
+      ret = E_OS_CALLEVEL;
+   }
+   else
 #endif
-	{
-		/* enter to critical code */
-		IntSecure_Start();
+   {
+      /* enter to critical code */
+      IntSecure_Start();
 
-		/* \req OSEK_SYS_3.16.1 The events of the extended task calling ClearEvent
-		 * are cleared according to the event mask Mask */
-		TasksVar[GetRunningTask()].Events &=
-			(EventMaskType)~( Mask & TasksConst[GetRunningTask()].EventsMask );
+      /* \req OSEK_SYS_3.16.1 The events of the extended task calling ClearEvent
+       * are cleared according to the event mask Mask */
+      TasksVar[GetRunningTask()].Events &=
+         (EventMaskType)~( Mask & TasksConst[GetRunningTask()].EventsMask );
 
-		/* finish cirtical code */
-		IntSecure_End();
+      /* finish cirtical code */
+      IntSecure_End();
 
-	}
+   }
 
 #if ( (ERROR_CHECKING_TYPE == ERROR_CHECKING_EXTENDED) && \
-		(HOOK_ERRORHOOK == ENABLE) )
-	/* \req OSEK_ERR_1.3-9/xx The ErrorHook hook routine shall be called if a
-	 * system service returns a StatusType value not equal to E_OK.*/
-	/* \req OSEK_ERR_1.3.1-9/xx The hook routine ErrorHook is not called if a
-	 * system service is called from the ErrorHook itself. */
+      (HOOK_ERRORHOOK == ENABLE) )
+   /* \req OSEK_ERR_1.3-9/xx The ErrorHook hook routine shall be called if a
+    * system service returns a StatusType value not equal to E_OK.*/
+   /* \req OSEK_ERR_1.3.1-9/xx The hook routine ErrorHook is not called if a
+    * system service is called from the ErrorHook itself. */
    if ( ( ret != E_OK ) && (ErrorHookRunning != 1))
-	{
-		SetError_Api(OSServiceId_ClearEvent);
-		SetError_Param1(Mask);
-		SetError_Ret(ret);
-		SetError_Msg("ClearEvent returns != than E_OK");
-		SetError_ErrorHook();
-	}
+   {
+      SetError_Api(OSServiceId_ClearEvent);
+      SetError_Param1(Mask);
+      SetError_Ret(ret);
+      SetError_Msg("ClearEvent returns != than E_OK");
+      SetError_ErrorHook();
+   }
 #endif
 
-	return ret;
+   return ret;
 }
 #endif /* #if (NO_EVENTS == DISABLE) */
 
