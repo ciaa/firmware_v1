43505763556bac79222b0fd88d1901bd9dae3b6a
Esteban Volentini <evolentini@equiser.com.ar>
2015-04-14 15:20:10 -0300

    #235 partial implementation of uart driver for fsl

---
+++ modules/drivers/cortexM4/k60_120/inc/ciaaDriverUart_Internal.h
@@ -0,0 +1,100 @@
+/* Copyright 2014, 2015 Mariano Cerdeiro, Esteban Volentini
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef _CIAADRIVERUART_INTERNAL_H_
+#define _CIAADRIVERUART_INTERNAL_H_
+/** \brief Internal Header file of DIO Driver
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup Drivers CIAA Drivers
+ ** @{ */
+/** \addtogroup DIO DIO Drivers
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe         Mariano Cerdeiro
+ * EV			Esteban Volentini
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20150403 v0.0.2 EV   first operational version
+ * 20140913 v0.0.1 MaCe first stub version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "ciaaPOSIX_stdint.h"
+#include "fsl_uart_driver.h"
+
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+/** \brief Uart Type */
+typedef struct {
+   uint32_t instance;               /** <= uart instance diver */
+	uart_user_config_t config;			/** <= driver configuration */
+	uart_state_t state;					/** <= driver status */
+} ciaaDriverUart_uartType;
+
+/*==================[external data declaration]==============================*/
+/** \brief Uart 0 */
+extern ciaaDriverUart_uartType ciaaDriverUart_uart0;
+
+/** \brief Uart 1 */
+extern ciaaDriverUart_uartType ciaaDriverUart_uart1;
+
+/** \brief Uart 2 */
+extern ciaaDriverUart_uartType ciaaDriverUart_uart2;
+
+/*==================[external functions declaration]=========================*/
+
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+}
+#endif
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _CIAADRIVERDIO_INTERNAL_H_ */
+
