e226412786f3df95b06938a989f7877935f24a07
Gustavo Muro <gustmuro@gmail.com>
2015-06-20 17:41:46 -0300

    #279 update Os_Cfg.h in modbus utest

--- modules/modbus/test/utest/inc/Os_Cfg.h
+++ modules/modbus/test/utest/inc/Os_Cfg.h
@@ -2,7 +2,7 @@
  * DO NOT CHANGE THIS FILE, IT IS GENERATED AUTOMATICALY*
  ********************************************************/
 
-/* Copyright 2008, 2009 Mariano Cerdeiro
+/* Copyright 2008, 2009, 2015 Mariano Cerdeiro
  * Copyright 2014, ACSE & CADIEEL
  *      ACSE: http://www.sase.com.ar/asociacion-civil-sistemas-embebidos/ciaa/
  *      CADIEEL: http://www.cadieel.org.ar
@@ -64,6 +64,7 @@
 /*
  * modification history (new versions first)
  * -----------------------------------------------------------
+ * 20150619 v0.1.4 MaCe fix issue #279
  * 20090719 v0.1.3 MaCe rename file to Os_
  * 20090424 v0.1.2 MaCe add counters defines
  * 20090128 v0.1.1 MaCe add MEMMAP off configuration
@@ -81,25 +82,29 @@
 /** \brief Task Definition */
 #define InitTask 0
 /** \brief Task Definition */
-#define ModbusSlave 1
+#define ModbusMaster 1
+/** \brief Task Definition */
+#define PollingSlave 2
 
 /** \brief Definition of the Application Mode AppMode1 */
 #define AppMode1 0
 
 /** \brief Definition of the Event POSIXE */
-#define POSIXE 1
+#define POSIXE 0x1U
 /** \brief Definition of the Event MODBUSE */
-#define MODBUSE 2
+#define MODBUSE 0x2U
 
 /** \brief Definition of the resource POSIXR */
-#define POSIXR 0
+#define POSIXR ((ResourceType)0)
 /** \brief Definition of the resource MODBUSR */
-#define MODBUSR 1
+#define MODBUSR ((ResourceType)1)
 
 /** \brief Definition of the Alarm ActivateModbusTask */
 #define ActivateModbusTask 0
+/** \brief Definition of the Alarm AlarmCallBackPollingSlave */
+#define AlarmCallBackPollingSlave 1
 /** \brief Definition of the Alarm IncrementSWCounter */
-#define IncrementSWCounter 1
+#define IncrementSWCounter 2
 
 /** \brief Definition of the Counter HardwareCounter */
 #define HardwareCounter 0
@@ -122,11 +127,33 @@
 
 #define OSErrorGetRet() (Osek_ErrorRet)
 
-/** \brief OSEK_MEMMAP macro (DISABLE not MemMap is used for FreeOSEK, ENABLE
+/** \brief OSEK_MEMMAP macro (OSEK_DISABLE not MemMap is used for FreeOSEK, OSEK_ENABLE
  ** MemMap is used for FreeOSEK) */
-#define OSEK_MEMMAP DISABLE
+#define OSEK_MEMMAP OSEK_DISABLE
+/** \brief Schedule this Task if higher priority Task are Active
+ **
+ ** \remarks if the system is configured with extended errors the
+ **          function Schedule is implemented as a macro and calls the
+ **          internal function Schedule_Int. If Standard errores are
+ **          configured the function Schedule is implemented as a function.
+ **
+ ** This API shall Schedule the calling Task if a higher priority Task
+ ** is active. This API shall only be used from non preemtive tasks.
+ **
+ ** \return E_OK if no error
+ ** \return E_OS_CALLEVEL if call at interrupt level
+ ** \return E_OS_RESOURCE if the calling task occupies resources
+ **/
+#define Schedule() Schedule_Int(TRUE)
 
 /*==================[typedef]================================================*/
+/** \brief Type definition of StatusType
+ **
+ ** This type is used to represent the status returned by all FreeOSEK APIs
+ **/
+/* \req OSEK_SYS_1.1 */
+typedef unsigned char StatusType;
+
 
 /*==================[external data declaration]==============================*/
 /** \brief Error Api Variable
@@ -170,11 +197,43 @@ extern void ErrorHook(void);
 
 /** \brief Task Declaration of Task InitTask */
 DeclareTask(InitTask);
-/** \brief Task Declaration of Task ModbusSlave */
-DeclareTask(ModbusSlave);
+/** \brief Task Declaration of Task ModbusMaster */
+DeclareTask(ModbusMaster);
+/** \brief Task Declaration of Task PollingSlave */
+DeclareTask(PollingSlave);
 
+/** \brief ISR Declaration */
+extern void OSEK_ISR_UART0_IRQHandler(void); /* Interrupt Handler UART0_IRQHandler */
+/** \brief ISR Declaration */
+extern void OSEK_ISR_UART2_IRQHandler(void); /* Interrupt Handler UART2_IRQHandler */
+/** \brief ISR Declaration */
+extern void OSEK_ISR_UART3_IRQHandler(void); /* Interrupt Handler UART3_IRQHandler */
 
+/** \brief Alarm Callback declaration */
+extern void OSEK_CALLBACK_CallBackActivatePollingSlave(void);
 
+/** \brief Schedule this Task if higher priority Task are Active
+ **
+ ** This API shall Schedule the calling Task if a higher priority Task
+ ** is active. This API shall only be used from non preemtive tasks.
+ **
+ ** \remarks This interface may be used by the end user over the
+ **          macro Schedule or from the system itself. Therefore
+ **          the parameter PerformChecks is provided. When
+ **          the user calls the scheduler the checks shall be
+ **          performed if the error checking is set as extended.
+ **          If the system calls the Schedule no error checking
+ **          shall be performed, the system shall be trusted.
+ **
+ ** \param[in] PerformChecks indicates if the function shall or not
+ **                          perform the extended checks. This parameter
+ **                          is only available if the error checks are set
+ **                          to extended.
+ ** \return E_OK if no error
+ ** \return E_OS_CALLEVEL if call at interrupt level
+ ** \return E_OS_RESOURCE if the calling task occupies resources
+ **/
+extern StatusType Schedule_Int(boolean PerformChecks);
 
 /** @} doxygen end group definition */
 /** @} doxygen end group definition */
