2736a5d04877325fbe9ebab0e1a13a69d021ac48
Carlos Pantelides <carlos.pantelides@gmail.com>
2015-07-06 21:22:20 -0300

    ciaa/Firmware#288 command line arguments handling code refactored to multiple functions

--- generator.php
+++ generator.php
@@ -197,6 +197,122 @@ function printCmdLine()
    print "\n";
 }
 
+function printInfo()
+{
+   print "INFO: ------ LICENSE START ------\n";
+   print "INFO: This file is part of CIAA Firmware.\n";
+   print "INFO: Redistribution and use in source and binary forms, with or without\n";
+   print "INFO: modification, are permitted provided that the following conditions are met:\n";
+   print "INFO: 1. Redistributions of source code must retain the above copyright notice,\n";
+   print "INFO: this list of conditions and the following disclaimer.\n";
+   print "INFO: 2. Redistributions in binary form must reproduce the above copyright notice,\n";
+   print "INFO: this list of conditions and the following disclaimer in the documentation\n";
+   print "INFO: and/or other materials provided with the distribution.\n";
+   print "INFO: 3. Neither the name of the copyright holder nor the names of its\n";
+   print "INFO: contributors may be used to endorse or promote products derived from this\n";
+   print "INFO: software without specific prior written permission.\n";
+   print "INFO: THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n";
+   print "INFO: AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n";
+   print "INFO: IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n";
+   print "INFO: ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE\n";
+   print "INFO: LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR\n";
+   print "INFO: CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF\n";
+   print "INFO: SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n";
+   print "INFO: INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN\n";
+   print "INFO: CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n";
+   print "INFO: ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE\n";
+   print "INFO: POSSIBILITY OF SUCH DAMAGE.\n";
+   print "INFO: ------- LICENSE END -------\n";
+}
+
+function printHelp()
+{
+   print "php generator.php [-l] [-h] [--cmdline] [-Ddef[=definition]] -c <CONFIG_1> [<CONFIG_2>] -o <OUTPUTDIR> -f <GENFILE_1> [<GENFILE_2>]\n";
+   print "      -c   indicate the configuration input files\n";
+   print "      -o   output directory\n";
+   print "      -f   indicates the templates to be processed\n";
+   print "   optional parameters:\n";
+   print "      -h   display this help\n";
+   print "      -l   displays a short license overview\n";
+   print "      -D   defines\n";
+   print "      --cmdline print the command line\n";
+}
+
+function processArgs($args)
+{
+   foreach ($args as $arg)
+   {
+      switch ($arg)
+      {
+      case "--cmdline":
+         printCmdLine();
+         break;
+      case "-l":
+         printInfo();
+         break;
+      case "-h":
+      case "--help":
+         printHelp();
+         exit();
+         break;
+      case "-v":
+         $verbose = true;
+         break;
+      case "-c":
+      case "-o":
+      case "-f":
+         $oldarg = $arg;
+         break;
+      default:
+         if (empty($oldarg))
+         {
+            if (0 == strpos("-D", $arg))
+            {
+               $tmp = explode("=", substr($arg, 2));
+               $definition[$tmp[0]] = $tmp[1];
+            }
+         } else {
+            switch($oldarg)
+            {
+            case "-c":
+               /* add a config file */
+               $configfiles[] = $arg;
+               break;
+            case "-o":
+               /* add an output dir */
+               $outputdir[]= $arg;
+               break;
+            case "-f":
+               /* add generated file */
+               $templatefiles[] = $arg;
+               break;
+            default:
+               halt("invalid argument: " . $arg);
+               break;
+            }
+         }
+         break;
+      }
+   }
+   
+   if (count($configfiles)==0)
+   {
+      halt("at least one config file shall be provided");
+   }
+
+   if (count($outputdir)!=1)
+   {
+      halt("exactly one output directory shall be provided");
+   }
+
+   if (count($templatefiles)==0)
+   {
+      halt("at least one tempalte file shall be provided");
+   }
+   
+   return array($verbose, $definition, $configfiles, $outputdir, $templatefiles);
+}
+
 $args = $_SERVER['argv'];
 $path = array_shift($args);
 
@@ -208,106 +324,7 @@ print "         ACSE : http://www.sase.com.ar/asociacion-civil-sistemas-embebido
 print "         CADIEEL: http://www.cadieel.org.ar\n";
 print "         All rights reserved.\n\n";
 
-foreach ($args as $arg)
-{
-   switch ($arg)
-   {
-   case "--cmdline":
-      printCmdLine();
-      break;
-   case "-l":
-      print "INFO: ------ LICENSE START ------\n";
-      print "INFO: This file is part of CIAA Firmware.\n";
-      print "INFO: Redistribution and use in source and binary forms, with or without\n";
-      print "INFO: modification, are permitted provided that the following conditions are met:\n";
-      print "INFO: 1. Redistributions of source code must retain the above copyright notice,\n";
-      print "INFO: this list of conditions and the following disclaimer.\n";
-      print "INFO: 2. Redistributions in binary form must reproduce the above copyright notice,\n";
-      print "INFO: this list of conditions and the following disclaimer in the documentation\n";
-      print "INFO: and/or other materials provided with the distribution.\n";
-      print "INFO: 3. Neither the name of the copyright holder nor the names of its\n";
-      print "INFO: contributors may be used to endorse or promote products derived from this\n";
-      print "INFO: software without specific prior written permission.\n";
-      print "INFO: THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n";
-      print "INFO: AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n";
-      print "INFO: IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n";
-      print "INFO: ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE\n";
-      print "INFO: LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR\n";
-      print "INFO: CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF\n";
-      print "INFO: SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n";
-      print "INFO: INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN\n";
-      print "INFO: CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n";
-      print "INFO: ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE\n";
-      print "INFO: POSSIBILITY OF SUCH DAMAGE.\n";
-      print "INFO: ------- LICENSE END -------\n";
-      break;
-   case "-h":
-   case "--help":
-      print "php generator.php [-l] [-h] [--cmdline] [-Ddef[=definition]] -c <CONFIG_1> [<CONFIG_2>] -o <OUTPUTDIR> -f <GENFILE_1> [<GENFILE_2>]\n";
-      print "      -c   indicate the configuration input files\n";
-      print "      -o   output directory\n";
-      print "      -f   indicates the templates to be processed\n";
-      print "   optional parameters:\n";
-      print "      -h   display this help\n";
-      print "      -l   displays a short license overview\n";
-      print "      -D   defines\n";
-      print "      --cmdline print the command line\n";
-      exit();
-      break;
-   case "-v":
-      $verbose = true;
-      break;
-   case "-c":
-   case "-o":
-   case "-f":
-      $oldarg = $arg;
-      break;
-   default:
-      if (empty($oldarg))
-      {
-         if (0 == strpos("-D", $arg))
-         {
-            $tmp = explode("=", substr($arg, 2));
-            $definition[$tmp[0]] = $tmp[1];
-         }
-      } else {
-         switch($oldarg)
-         {
-         case "-c":
-            /* add a config file */
-            $configfiles[] = $arg;
-            break;
-         case "-o":
-            /* add an output dir */
-            $outputdir[]= $arg;
-            break;
-         case "-f":
-            /* add generated file */
-            $templatefiles[] = $arg;
-            break;
-         default:
-            halt("invalid argument: " . $arg);
-            break;
-         }
-      }
-      break;
-   }
-}
-
-if (count($configfiles)==0)
-{
-   halt("at least one config file shall be provided");
-}
-
-if (count($outputdir)!=1)
-{
-   halt("exactly one output directory shall be provided");
-}
-
-if (count($templatefiles)==0)
-{
-   halt("at least one tempalte file shall be provided");
-}
+list($verbose, $definition, $configfiles, $outputdir, $templatefiles)= processArgs($args);
 
 if ($verbose)
 {
