02835c05e97931ca63067711d78100b250a3f323
Juan Manuel Cruz <juanmanuelcruzbeaufrere@gmail.com>
2016-01-18 20:04:20 -0300

    #374 remove Tabs

--- modules/hisio/src/cortexM4/lpc43xx/lpc4337/Dio_Arch.c
+++ modules/hisio/src/cortexM4/lpc43xx/lpc4337/Dio_Arch.c
@@ -89,13 +89,13 @@ extern IO_ErrorType Dio_InitSync_Arch(void * address)
       }
       else if(Dio_Config.Pins[i].Flags & DIO_CONFIG_PIN_DIRECTION_OUTPUT_INIT_LOW)
       {
-     	 Chip_SCU_PinMux(Dio_Config.Pins[i].Port, Dio_Config.Pins[i].Pin, SCU_MODE_INACT | SCU_MODE_INBUFF_EN | SCU_MODE_ZIF_DIS, Dio_Config.Pins[i].GPIO_Func);
+         Chip_SCU_PinMux(Dio_Config.Pins[i].Port, Dio_Config.Pins[i].Pin, SCU_MODE_INACT | SCU_MODE_INBUFF_EN | SCU_MODE_ZIF_DIS, Dio_Config.Pins[i].GPIO_Func);
          Chip_GPIO_SetDir(LPC_GPIO_PORT, Dio_Config.Pins[i].GPIO_Port, (1 << Dio_Config.Pins[i].GPIO_Pin), 1); /* Set it as output */
          Chip_GPIO_SetPinState(LPC_GPIO_PORT, Dio_Config.Pins[i].GPIO_Port, Dio_Config.Pins[i].GPIO_Pin, 0); /* Set it as low */
       }
       else if(Dio_Config.Pins[i].Flags & DIO_CONFIG_PIN_DIRECTION_OUTPUT_INIT_HIGH)
       {
-    	 Chip_SCU_PinMux(Dio_Config.Pins[i].Port, Dio_Config.Pins[i].Pin, SCU_MODE_INACT | SCU_MODE_INBUFF_EN | SCU_MODE_ZIF_DIS, Dio_Config.Pins[i].GPIO_Func);
+         Chip_SCU_PinMux(Dio_Config.Pins[i].Port, Dio_Config.Pins[i].Pin, SCU_MODE_INACT | SCU_MODE_INBUFF_EN | SCU_MODE_ZIF_DIS, Dio_Config.Pins[i].GPIO_Func);
          Chip_GPIO_SetDir(LPC_GPIO_PORT, Dio_Config.Pins[i].GPIO_Port, (1 << Dio_Config.Pins[i].GPIO_Pin), 1); /* Set it as output */
          Chip_GPIO_SetPinState(LPC_GPIO_PORT, Dio_Config.Pins[i].GPIO_Port, Dio_Config.Pins[i].GPIO_Pin, 1); /* Set it as high */
       }
@@ -109,7 +109,6 @@ extern IO_ValueType Dio_GetSync_Arch(IO_ChannelType channel)
 {
    IO_ValueType value;
 #if (ERROR_CHECKING_TYPE == ERROR_CHECKING_EXTENDED)   
-//   if(DIO_PINS_COUNT > channel && (Dio_Config.Pins[channel].Flags & DIO_CONFIG_PIN_DIRECTION_INPUT))
    if(DIO_PINS_COUNT > channel)
 #endif
    {
